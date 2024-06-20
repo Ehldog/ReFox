36a6bac4bf437bdb7f47bb223b5b246aec2075a6
Sapo_Boy <70650786+Sapo-Boy@users.noreply.github.com>
2024-06-20 21:13:28 +0200

    citywork

--- Level/City/City_lvl.gd
+++ Level/City/City_lvl.gd
@@ -63,7 +63,7 @@ func _process(delta):
 			ground_choice.position.x += screen_size.x - 100
 		
 		# score
-		score += speed
+		score += speed 
 		show_score()
 		
 		# emit signal
@@ -75,7 +75,7 @@ func _process(delta):
 
  #obstacle spwaner (FAIRE LES COMMENTAIRES !!)
 func generate_obs():
-	if obstacles.is_empty() or last_obs.position.x < score + randi_range(500, 600):
+	if obstacles.is_empty() or (last_obs.position.x - player_choice.position.x ) < score + randi_range(300, 500):
 		var obs_type = obstacle_types[randi() % obstacle_types.size()]
 		var obs
 		obs = obs_type.instantiate()
@@ -84,7 +84,7 @@ func generate_obs():
 		var obs_x: int = screen_size.x + score + 100
 		var obs_y: int = screen_size.y - ground_height - (obs_height.y/2) + 15
 		last_obs = obs
-		add_obs(obs, obs_x, obs_y)
+		add_obs(obs, obs_x + player_choice.position.x, obs_y)
 
 func add_obs(obs, x, y):
 	obs.position = Vector2i(x, y)
