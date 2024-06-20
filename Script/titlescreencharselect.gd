791a3d9632c2c53f63ece27d9737405787e76e6f
Sapo_Boy <saporito.gaetan.04@gmail.com>
2024-06-20 18:00:04 +0200

    charselecthing

--- Script/titlescreencharselect.gd
+++ Script/titlescreencharselect.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var transition = $MovementParralax/transition
 @onready var SelectNoise = $Select
 
+var stage = false
+
 func _on_bck_pressed():
 	SelectNoise.play()
 	transition.play("fade")
@@ -10,3 +12,18 @@ func _on_bck_pressed():
 
 func _on_transition_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://titlescreen.tscn")
+
+
+func _on_button_player_pressed():
+	Global.joueur = false
+	print(Global.joueur)
+
+
+func _on_button_player_2_pressed():
+	Global.joueur = true
+	print(Global.joueur)
+
+
+func _on_check_button_pressed():
+	stage = !stage
+	print(stage)
