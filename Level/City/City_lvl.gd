ffc3f9fce0fd23cb3c40b92d3c1c05ddcc5db52e
Sapo_Boy <saporito.gaetan.04@gmail.com>
2024-06-20 17:22:49 +0200

    MobFix

--- Level/City/City_lvl.gd
+++ Level/City/City_lvl.gd
@@ -5,10 +5,10 @@ extends Node2D
 @export var ground_choice = StaticBody2D
 
 # preload obstacle ( spawner directement dans le code faute de temps)
-#var pig_assassin = preload("res://Characters/Monsters/Pig_Assassin.tscn")
-#var troll = preload("res://Characters/Monsters/Troll.tscn")
-#var obstacle_types := [pig_assassin, troll]
-#var obstacles: Array
+var pig_assassin = preload("res://Scenes/Monsters/Pig_Assassin.tscn")
+var troll = preload("res://Scenes/Monsters/Pig_Assassin.tscn")
+var obstacle_types := [pig_assassin, troll]
+var obstacles: Array
 
 # game variables & const
 const PLAYER_START_POS := Vector2i(60, 294)
@@ -51,7 +51,7 @@ func _process(delta):
 			speed = MAX_SPEED
 		
 		# obstacle spwaner
-		#generate_obs()
+		generate_obs()
 		
 		# Move player
 		player_choice.position.x += speed
@@ -71,15 +71,17 @@ func _process(delta):
 			game_running = true
 			$Hud.get_node("SpaceLabel").hide()
 
-# obstacle spwaner (FAIRE LES COMMENTAIRES !!)
-#func generate_obs():
-	#if obstacles.is_empty():
-		#var obs_type = obstacle_types[randi() % obstacle_types.size()]
-		#var obs
-		#obs = obs_type.instantiate()
-		#last_obs = obs
-		#add_child(obs)
-		#obstacles.append(obs)
+ #obstacle spwaner (FAIRE LES COMMENTAIRES !!)
+func generate_obs():
+	if obstacles.is_empty():
+		var obs_type = obstacle_types[randi() % obstacle_types.size()]
+		var obs
+		obs = obs_type.instantiate()
+		var spriteheight = obs.get_node("AnimatedSprite2D").sprite_frames.get_frame_texture("idle", 0)
+		var obs_height = spriteheight.get_size()
+		last_obs = obs
+		add_child(obs)
+		obstacles.append(obs)
 		
 func show_score():
 	$Hud.get_node("ScoreLabel").text = " Score: " + str(score / SCORE_MODIFIER)
